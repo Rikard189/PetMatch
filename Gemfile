ed180128a802e273185413bbd7272b2eb4cb73e7
Ricardo Acosta <rikard189@gmail.com>
2018-11-20 16:31:29 -0600

    Change style of pets index view

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem "font-awesome-rails"
 
 gem 'carrierwave', '~> 1.0'
 
+gem 'pry-rails', group: [:development, :test]
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module PetMatch
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    The asset "img1.jpeg" is not present in the asset pipeline.)
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
